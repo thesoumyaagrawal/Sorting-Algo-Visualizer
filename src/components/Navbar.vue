7f16233d78d31041cbd5154d9ae58794f1a30171
Riyomi <sz.richard98@gmail.com>
2021-07-17 22:26:09 +0200

    Cleaned up generated HTML files]

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,4 +1,9 @@
-<template> </template>
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+</template>
 
 <script>
 export default {
